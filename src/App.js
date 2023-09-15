7fabe6366c66d1222d30ca6bd2bd2b47b7da4344
TatzkDepression <tatzkrebecca@gmail.com>
2023-09-15 08:37:00 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,36 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
-import Header from "./Content/Header";
 import Content from "./Content/Content";
 
 function App() {
   return (
     <div className="App">
-      <div
-        className="gray"
-        style={{
-          position: "absolute",
-          height: "100vh",
-          width: "100vw",
-          top: 0,
-          left: 0,
-          backgroundColor: "rgba(0, 0, 0, 0.5)",
-          zIndex: -1,
-        }}
-      ></div>
-
-      <div
-        className="background"
-        style={{
-          position: "absolute",
-          backgroundImage: "url(./glassesImage/background.jpg)",
-          height: "100vh",
-          width: "100vw",
-          backgroundSize: "cover",
-          zIndex: -2,
-        }}
-      ></div>
-
+      <div className="gray"></div>
+      <div className="background"></div>
       <div className="container">
         <Content />
       </div>
